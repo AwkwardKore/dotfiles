85d0a4c814d7eb7bd1890da49d458cc18408d671
Rolando Islas <dani.islas96@gmail.com>
2021-09-04 18:40:38 -0500

    Remove unused zsh aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,10 +2,6 @@
 alias vimconfig="vim ~/.vimrc"
 alias zshconfig="vim ~/.zshrc"
 
-# Save and fetch dotfiles
-alias gpvimconfig="(cd ~/dotfiles && git add vim/.vimrc && git commit && git push origin master)"
-alias gfdotfiles="(cd ~/dotfiles && git pull origin master)"
-
 # Swap files for vim
 alias findswp="find . -type f -name '*.swp'"
 alias delswp="find . -type f -name '*.swp' -delete"

97a0a71bee16ffdae5651f894b33160f256110ef
Rolando Islas <dani.islas96@gmail.com>
2022-04-27 20:22:03 -0500

    Add alias for running command with rosetta

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -5,3 +5,6 @@ alias zshconfig="vim ~/.zshrc"
 # Swap files for vim
 alias findswp="find . -type f -name '*.swp'"
 alias delswp="find . -type f -name '*.swp' -delete"
+
+# Apple Silicon
+alias rosetta="arch -x86_64"
